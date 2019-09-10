83360b2e048fcb893a1e0472c72fe002237b86bb
Max Axenov <maksenov@wavesplatform.com>
2019-09-10 12:08:28 +0300

    change parse-json-bignumber to @waves/parse-json-bignumber

--- src/jsonMethods.ts
+++ src/jsonMethods.ts
@@ -1,4 +1,4 @@
-import * as create from 'parse-json-bignumber/dist/parse-json-bignumber'
+import * as create from '@waves/parse-json-bignumber/dist/parse-json-bignumber'
 
 const {parse, stringify} = create()
 import {getTransactionSchema, orderSchemaV1, orderSchemaV2} from './schemas'
